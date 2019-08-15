fe6d6a2578e40ac85fd3334cb2fd421bae44b40d
李强进 <li.qj@ikcrm.com>
2019-08-15 18:50:24 +0800

    修改

--- src/pages/Student/Student.jsx
+++ src/pages/Student/Student.jsx
@@ -1,59 +1,59 @@
-import React, { useState, useEffect } from "react";
-import { withRouter } from "react-router-dom";
-import { Card, Button, message, Alert } from "antd";
+import React, {useEffect, useState} from "react";
+import {withRouter} from "react-router-dom";
+import {Alert, Button, Card, message} from "antd";
 import service from "../../services/index";
-import { transformTime } from "../../utils/time";
+import {transformTime} from "../../utils/time";
 
 const Student = () => {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  const getStudent = () => {
-    service("getMyStudents", {}).then(res => {
-      if (res.code === 0) {
-        setData(res.data);
-      }
-    });
-  };
+    const getStudent = () => {
+        service("getMyStudents", {}).then(res => {
+            if (res.code === 0) {
+                setData(res.data);
+            }
+        });
+    };
 
-  const submit = id => {
-    service("review", { studentIds: [id] }).then(res => {
-      if (res.code === 0) {
-        message.info("该学生已通过审核～");
+    const submit = id => {
+        service("review", {studentIds: [id]}).then(res => {
+            if (res.code === 0) {
+                message.info("该学生已通过审核");
+                getStudent();
+            }
+        });
+    };
+
+    useEffect(() => {
         getStudent();
-      }
-    });
-  };
+    }, []);
 
-  useEffect(() => {
-    getStudent();
-  }, []);
-
-  return (
-    <div className="student">
-      <Alert
-        message="新学生选择您为老师后需要您的审核才能做题哦，点击下方按钮通过审核吧～"
-        type="info"
-        style={{ marginBottom: "20px" }}
-      />
-      <Card>
-        {data.map(({ student }) => (
-          <Card.Grid
-            key={student.id}
-            style={{ textAlign: "center", width: "25%" }}
-          >
-            <div style={{ marginBottom: "10px" }}>ID：{student.id}</div>
-            <div style={{ marginBottom: "10px" }}>姓名：{student.name}</div>
-            <div style={{ marginBottom: "10px" }}>
-              申请时间：{transformTime(student.createdAt)}
-            </div>
-            <Button type="primary" onClick={() => submit(student.id)}>
-              通过审核
-            </Button>
-          </Card.Grid>
-        ))}
-      </Card>
-    </div>
-  );
+    return (
+        <div className="student">
+            <Alert
+                message="新学生选择您为老师后需要您的审核才能做题哦，点击下方按钮通过审核吧～"
+                type="info"
+                style={{marginBottom: "20px"}}
+            />
+            <Card>
+                {data.map(({student}) => (
+                    <Card.Grid
+                        key={student.id}
+                        style={{textAlign: "center", width: "25%"}}
+                    >
+                        <div style={{marginBottom: "10px"}}>ID：{student.id}</div>
+                        <div style={{marginBottom: "10px"}}>姓名：{student.name}</div>
+                        <div style={{marginBottom: "10px"}}>
+                            申请时间：{transformTime(student.createdAt)}
+                        </div>
+                        <Button type="primary" onClick={() => submit(student.id)}>
+                            通过审核
+                        </Button>
+                    </Card.Grid>
+                ))}
+            </Card>
+        </div>
+    );
 };
 
 export default withRouter(Student);
--- src/pages/User/RegisteResult.js
+++ src/pages/User/RegisteResult.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Entry from '../../layout/Entry'
 
 function RegisteResult() {
-  return (
-    <Entry>
-      <div style={{ fontSize: '20px' }}>注册申请已提交，请静候管理员审核。</div>
-    </Entry>
-  )
+    return (
+        <Entry>
+            <div style={{fontSize: '20px'}}>注册申请已提交，请通知管理员审核。</div>
+        </Entry>
+    )
 }
 
 export default RegisteResult
